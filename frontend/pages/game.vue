772b6e58b298a7ea71abcbe2499571a717d203b4
root <root@DESKTOP-24DGNC6.localdomain>
2026-06-08 19:02:20 +0800

    auto: 06-08 19:02 — frontend/pages/game.vue

--- frontend/pages/game.vue
+++ frontend/pages/game.vue
@@ -94,7 +94,6 @@
     
     <WikiModal :show="showWiki" :realm-id="player.realmId" :player-spirit-name="player.spiritName" @close="showWiki=false" />
       <GameModals :pve-report="pveReport" :pve-rounds="pveRounds" :encounter-result="encounterResult" :is-dead="isDead" :revive-countdown="reviveCountdown" :death-log="deathLog" :gender="player.gender" :hp="player.maxHp>0?Math.round(player.hp/player.maxHp*100):0" :mp="player.maxMp>0?Math.round(player.mp/player.maxMp*100):0" :spirit-sense="player.spiritSense" @update:pve-report="(v:any)=>pveReport=v" @update:pve-rounds="(v:any)=>pveRounds=v" @update:encounter-result="(v:any)=>encounterResult=v" />
-    </Teleport>
   </div>
 </template>
 
